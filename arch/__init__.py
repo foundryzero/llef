39c96644e228609703f2e98c44d37ff182c9c571
stephen-f0 <116006802+stephen-f0@users.noreply.github.com>
2023-08-19 16:40:07 +0100

    Add arm (32-bit) support

--- arch/__init__.py
+++ arch/__init__.py
@@ -4,6 +4,7 @@ from typing import Type
 from lldb import SBTarget
 
 from arch.aarch64 import Aarch64
+from arch.arm import Arm
 from arch.base_arch import BaseArch
 from arch.x86_64 import X86_64
 from common.constants import MSG_TYPE
@@ -11,7 +12,12 @@ from common.util import extract_arch_from_triple, print_message
 
 # macOS devices running arm chips identify as arm64.
 # aarch64 and arm64 backends have been merged, so alias arm64 to aarch64
-supported_arch = {"x86_64": X86_64, "aarch64": Aarch64, "arm64": Aarch64}
+supported_arch = {
+    "arm": Arm,
+    "x86_64": X86_64,
+    "aarch64": Aarch64,
+    "arm64": Aarch64,
+}
 
 
 def get_arch(target: SBTarget) -> Type[BaseArch]:
