7495cfaf75554851d0dcb9df7bdb20f2e13243c9
Nikodem Rabuliński <nikodem@rabulinski.com>
2023-10-18 14:50:30 +0200

    Add support for debugging Apple binaries by aliasing arm64e to Aarch64

--- arch/__init__.py
+++ arch/__init__.py
@@ -11,14 +11,16 @@ from common.constants import MSG_TYPE
 from common.util import extract_arch_from_triple, print_message
 
 # macOS devices running arm chips identify as arm64.
-# aarch64 and arm64 backends have been merged, so alias arm64 to aarch64
+# aarch64 and arm64 backends have been merged, so alias arm64 to aarch64.
+# There's also arm64e architecture, which is basically ARMv8.3
+# but includes pointer authentication and for now is Apple-specific.
 supported_arch = {
     "arm": Arm,
     "x86_64": X86_64,
     "aarch64": Aarch64,
     "arm64": Aarch64,
+    "arm64e": Aarch64,
 }
-
 
 def get_arch(target: SBTarget) -> Type[BaseArch]:
     """Get the architecture of a given target"""
