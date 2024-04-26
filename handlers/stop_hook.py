644868a8e8543b439ed18da18fb2fbf901e5a80e
stephen-f0 <116006802+stephen-f0@users.noreply.github.com>
2024-04-26 08:15:30 +0100

    Initial global state implementation

--- handlers/stop_hook.py
+++ handlers/stop_hook.py
@@ -1,31 +1,21 @@
 """Break point handler."""
-from typing import Any, Dict, Type
+from typing import Any, Dict
 
 from lldb import (
     SBDebugger,
     SBExecutionContext,
-    SBFrame,
-    SBProcess,
     SBStream,
     SBStructuredData,
     SBTarget,
-    SBThread,
 )
 
-from arch.base_arch import BaseArch
 from common.context_handler import ContextHandler
 
 
 class StopHookHandler:
     """Stop Hook handler."""
 
-    frame: SBFrame
-    process: SBProcess
-    target: SBTarget
-    thread: SBThread
-    arch: Type[BaseArch]
-
-    old_registers: Dict[str, int] = {}
+    context_handler: ContextHandler
 
     @classmethod
     def lldb_self_register(cls, debugger: SBDebugger, module_name: str) -> None:
@@ -38,14 +28,9 @@ class StopHookHandler:
         self, target: SBTarget, _: SBStructuredData, __: Dict[Any, Any]
     ) -> None:
         """
-        This is probably where a global state object should be initiated. Current version only uses
-        class scoped state (e.g. self.old_registers). The limitation of this is that `commands` can't
-        interact with state.
-
         For up to date documentation on args provided to this function run: `help target stop-hook add`
         """
-        self.target = target
-        self.context_handler = ContextHandler(self.target.debugger)
+        self.context_handler = ContextHandler(target.debugger)
 
     def handle_stop(self, exe_ctx: SBExecutionContext, _: SBStream) -> None:
         """For up to date documentation on args provided to this function run: `help target stop-hook add`"""
